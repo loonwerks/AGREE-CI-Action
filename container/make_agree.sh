bb527297c60dd72e9d35f0a428c9e9f752ff1ad8
Karl Hoech <karl.hoech@collins.com>
2024-06-06 12:01:34 -0500

    Fix osate version, pin agree version

--- container/make_agree.sh
+++ container/make_agree.sh
@@ -4,10 +4,11 @@ set -Eeuxo pipefail
 
 : "${FMIDE_DIR:=fmide}"
 : "${eclipseRelease:=2021-03}"
-: "${OSATE_VERSION:=2.10.2}"
-: "${OSATE_URL:=https://osate-build.sei.cmu.edu/download/osate/stable/${OSATE_VERSION}/products/osate2-${OSATE_VERSION}-vfinal-linux.gtk.x86_64.tar.gz}"
+: "${OSATE_VERSION:=2.10.2-vfinal}"
+: "${OSATE_URL:=https://osate-build.sei.cmu.edu/download/osate/stable/${OSATE_VERSION}/products/osate2-${OSATE_VERSION}-linux.gtk.x86_64.tar.gz}"
 : "${AGREE_UPDATE_SITE:=https://raw.githubusercontent.com/loonwerks/AGREE-Updates/master/,http://ca-trustedsystems-dev-us-east-1.s3-website-us-east-1.amazonaws.com/p2/snapshots/agree/}"
 : "${AGREE_FEATURE_ID:=com.rockwellcollins.atc.agree.feature.feature.group}"
+: "${AGREE_VERSION:=2.9.1}"
 
 rm -rf ${FMIDE_DIR}
 mkdir -p ${FMIDE_DIR}
@@ -15,4 +16,4 @@ pushd ${FMIDE_DIR}
 curl ${OSATE_URL} | tar xz
 popd
 
-${FMIDE_DIR}/osate -nosplash -console -consoleLog -application org.eclipse.equinox.p2.director -repository ${AGREE_UPDATE_SITE} -installIU ${AGREE_FEATURE_ID}
+${FMIDE_DIR}/osate -nosplash -console -consoleLog -application org.eclipse.equinox.p2.director -repository ${AGREE_UPDATE_SITE} -installIU ${AGREE_FEATURE_ID}/${AGREE_VERSION}
