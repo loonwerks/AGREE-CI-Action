645998eb2a050c9786bd261ca3db03d2b3a92d82
Karl Hoech <karl.hoech@collins.com>
2024-06-10 15:09:20 -0500

    Use absolute path for output in entrypoint.sh and add instrumentation

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,10 @@ echo "project-name: $2"
 echo "component-to-analyze: $3"
 echo "analysis-strategy: $4"
 echo "optional-flags: $5"
-xvfb-run -e /dev/stdout -s "-screen 0 1280x1024x24 -ac -nolisten tcp -nolisten unix" /fmide/osate -application com.rockwellcollins.atc.agree.cli.Agree -noSplash -data $GITHUB_WORKSPACE/$1 -p $2 -c $3 -strategy $4 -o AGREE_output.json $5
+ls /github/workspace
+ls -la .
+xvfb-run -e /dev/stdout -s "-screen 0 1280x1024x24 -ac -nolisten tcp -nolisten unix" /fmide/osate -application com.rockwellcollins.atc.agree.cli.Agree -noSplash -data $GITHUB_WORKSPACE/$1 -p $2 -c $3 -strategy $4 -o /github/workspace/AGREE_output.json $5
 exitStatus=$?
-cat AGREE_output.json >> $GITHUB_OUTPUT
+cat /github/workspace/AGREE_output.json >> $GITHUB_OUTPUT
 exit $exitStatus
 
